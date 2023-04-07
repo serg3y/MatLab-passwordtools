68584bd0ff07d8b123b0e6e44d9182d67f17f857
Serge <s3rg3y@hotmail.com>
2023-04-08 02:13:28 +0930

    Added getloin

---
+++ getlogin.m
@@ -0,0 +1,48 @@
+function [user,pass] = getlogin(valid,heading)
+%Show username and password dialogue (hide password).
+% [user,pass] = getlogin        -display dialogue
+% [user,pass] = getlogin(valid)     -allowed characters (default: char(32:126))
+% [user,pass] = getlogin(valid,heading) -dialogue heading (default: 'Login')
+%
+%Remarks:
+%-WindowStyle='modal' blocks user interaction with other UI elements.
+%
+%Example:
+% [user,pass] = getlogin([],'My Program Login')
+
+if nargin<1 || isempty(valid), valid = char(32:126); end %default charset
+if nargin<2 || isempty(heading), heading = 'Login'; end %default heeding
+
+sz = get(0,'ScreenSize');
+hFig  = figure(WindowStyle='modal' ,Position=[(sz(3:4)-[350 100])/2 350 100],Name=heading,Resize='off',NumberTitle='off',Menubar='none',Color=[0.9 0.9 0.9],CloseRequestFcn=@(~,~)uiresume);
+hUser = uicontrol(hFig,Style='edit',Position=[80 60 250 20],KeyPressFcn=@userKeyPress,FontSize=10,BackGroundColor='w');
+hPass = uicontrol(hFig,Style='text',Position=[80 30 250 20],ButtonDownFcn=@passClick,FontSize=10,BackGroundColor='w',Enable='Inactive');
+hDumy = uicontrol(hFig,Style='push',Position=[ 0  0   0  0],KeyPressFcn=@passKeyPress); %button represents password text box, to simulate tab behaviour
+annotation(hFig,'textbox',Units='pixels',Position=[00 60 80 20],String='Username',EdgeColor='n',VerticalAlignment='middle',HorizontalAlignment='right')
+annotation(hFig,'textbox',Units='pixels',Position=[00 30 80 20],String='Password',EdgeColor='n',VerticalAlignment='middle',HorizontalAlignment='right')
+pass = ''; %init password
+uicontrol(hUser) %give focuse to username
+uiwait %wait for uiresume command
+drawnow %required for next line
+user = hUser.String;
+delete(hFig) %clean up
+
+    function passClick(~,~)
+        uicontrol(hDumy)
+    end
+    function userKeyPress(~,event)
+        if contains(event.Key,{'return' 'escape'})
+            uiresume, return %finish
+        end
+    end
+    function passKeyPress(~,event)
+        if event.Key=="backspace"
+            pass = pass(1:end-1); %shorten password
+        elseif contains(event.Key,{'return' 'escape'})
+            uiresume, return %done
+        elseif contains(event.Character,num2cell(char(valid)))
+            pass(end+1) = event.Character; %append key to password
+        end
+        hPass.String = repmat(char(8226),size(pass)); %display dots
+    end
+end
--- getpass.m
+++ getpass.m
@@ -1,4 +1,4 @@
-function pass = getpass(str,lbl)
+function pass = getpass(valid,heading)
 %Create a dialogue for user to enter password while hiding the input.
 % pass = getpass       -display dialogue
 % pass = getpass(str)    -list of valid characters (default: char(32:126))
@@ -6,12 +6,15 @@ function pass = getpass(str,lbl)
 %
 %Remarks:
 %-WindowStyle='modal' blocks user interaction with other UI elements.
+%
+%Example:
+% pass = getpass('0123456789','Enter PIN') 
 
-if nargin<1 || isempty(str), str = char(32:126); end %default charset
-if nargin<2 || isempty(lbl), lbl = 'Enter Password'; end %default heeding
+if nargin<1 || isempty(valid), valid = char(32:126); end %default charset
+if nargin<2 || isempty(heading), heading = 'Enter Password'; end %default heeding
 
 ScreenSize = get(0,'ScreenSize');
-hfig  = figure(WindowStyle='modal' ,Units='pixels',Position=[(ScreenSize(3:4)-[300 75])/2 300 75],Name=lbl,Resize='off',NumberTitle='off',Menubar='none',KeyPressFcn=@keypress,CloseRequestFcn=@(~,~)uiresume);
+hfig  = figure(WindowStyle='modal' ,Units='pixels',Position=[(ScreenSize(3:4)-[300 75])/2 300 75],Name=heading,Resize='off',NumberTitle='off',Menubar='none',KeyPressFcn=@keypress,CloseRequestFcn=@(~,~)uiresume);
 hpass = uicontrol(hfig,Style='text',Units='pixels',Position=[20 30 260 20],FontSize=10,BackGroundColor='w');
 hwarn = uicontrol(hfig,Style='text',Units='pixels',Position=[50  2 200 20],FontSize=10);
 pass = ''; %init
@@ -19,17 +22,18 @@ uiwait %hold program execution
 delete(hfig) %clean up
 
     function keypress(~,event)
-        switch event.Key
-            case 'backspace'
-                pass = pass(1:end-1); %shorten password
-            case {'return' 'escape'} %Enter or ESC was pressed
-                uiresume, return %exit
-            case num2cell(char(str))
-                pass(end+1) = event.Key; %append key to password
-            otherwise
-                hwarn.String='forbidden character'; %warn user
-                pause(0.5);
-                hwarn.String=''; 
+        if event.Key=="backspace"
+            pass = pass(1:end-1); %shorten password
+        elseif contains(event.Key,{'return' 'escape'}) %Enter or ESC was pressed
+            uiresume, return %exit
+        elseif event.Key=="shift"
+            %do nothing
+        elseif contains(event.Character,num2cell(char(valid)))
+            pass(end+1) = event.Character; %append key to password
+        else
+            hwarn.String = 'Invalid character'; %warn user
+            pause(0.5);
+            hwarn.String = '';
         end
         hpass.String = repmat(char(8226),size(pass)); %display dots instead of password
     end
