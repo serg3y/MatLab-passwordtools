f5eb29b0f5767e5251348859ee4ae0d786e2defd
Serge <s3rg3y@hotmail.com>
2023-04-08 02:21:55 +0930

    Update getpass.m

--- getpass.m
+++ getpass.m
@@ -11,12 +11,12 @@ function pass = getpass(valid,heading)
 % pass = getpass('0123456789','Enter PIN') 
 
 if nargin<1 || isempty(valid), valid = char(32:126); end %default charset
-if nargin<2 || isempty(heading), heading = 'Enter Password'; end %default heeding
+if nargin<2 || isempty(heading), heading = 'Enter password'; end %default heeding
 
 ScreenSize = get(0,'ScreenSize');
-hfig  = figure(WindowStyle='modal' ,Units='pixels',Position=[(ScreenSize(3:4)-[300 75])/2 300 75],Name=heading,Resize='off',NumberTitle='off',Menubar='none',KeyPressFcn=@keypress,CloseRequestFcn=@(~,~)uiresume);
+hfig  = figure(WindowStyle='modal' ,Units='pixels',Position=[(ScreenSize(3:4)-[300 75])/2 300 75],Name=heading,Resize='off',NumberTitle='off',Menubar='none',Color=[0.9 0.9 0.9],KeyPressFcn=@keypress,CloseRequestFcn=@(~,~)uiresume);
 hpass = uicontrol(hfig,Style='text',Units='pixels',Position=[20 30 260 20],FontSize=10,BackGroundColor='w');
-hwarn = uicontrol(hfig,Style='text',Units='pixels',Position=[50  2 200 20],FontSize=10);
+hwarn = uicontrol(hfig,Style='text',Units='pixels',Position=[50  2 200 20],FontSize=10,BackGroundColor=[0.9 0.9 0.9]);
 pass = ''; %init
 uiwait %hold program execution
 delete(hfig) %clean up
